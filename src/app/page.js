e0939c262c29d4dce14ba2d4d2ab1b89d6405376
MarloweReyes <mreyes58@my.bcit.ca>
2025-02-01 16:22:57 -0800

    adding mockup video to landing page

--- src/app/page.js
+++ src/app/page.js
@@ -11,17 +11,25 @@ export default function Home() {
       <Header />
       <Hero />
       <section className={styles.intro}>
-      <div className={styles.container}>
-        <div className={styles.introText}>
-          <h3>
-            <span className={styles.redBold}>UX/UI & Graphic Designer</span>
-            —unconventionally bringing stories to life through digital design, crafting impactful digital solutions with a unique perspective rooted in creativity and purpose.
-          </h3>
-          <Link href="/about" className={styles.link1}>
-            Learn more about me ↗
-          </Link>
+        <div className={styles.container}>
+          <div className={styles.introText}>
+            <h3>
+              <span className={styles.redBold}>UX/UI & Graphic Designer</span>
+              —unconventionally bringing stories to life through digital design,
+              crafting impactful digital solutions with a unique perspective
+              rooted in creativity and purpose.
+            </h3>
+            <Link href="/about" className={styles.link1}>
+              Learn more about me ↗
+            </Link>
+          </div>
         </div>
-        </div>
+      </section>
+      <section className={styles.mockupVids}>
+        <video className={styles.video} autoPlay loop muted playsInline>
+          <source src="/images/mock-vid.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </section>
     </>
   );
